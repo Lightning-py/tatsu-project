dfd04983318e1effbf649d5b1c2b3e6ec2350af4
sergey <sergejananev78@gmail.com>
2021-11-19 01:48:19 +0300

    добавляет функциональность альф и всего прочего, вроде даже работает

--- main.py
+++ main.py
@@ -3,85 +3,84 @@ from pprint import pprint
 
 from math import factorial, sin, cos, pi, e, radians
 
+
 import tatsu
 
+''' 
+я придумал небольшой костыль, теперь вместо того чтобы создавать python переменные мы будем просто создавать словарь,
+в котором будем хранить все переменные вместе с именами
+'''
+
+variables_dict = {}
 
 class CalcBasicSemantics(object):
-    def math_pi(self, ast):
-        return pi
 
-    def math_e(self, ast):
-        return e
+    def temporary_string(self, ast):
+        return ast
 
-    def number_float(self, ast):
-        return float(ast)
 
-    def number_int(self, ast):
-        return int(ast)
+    def element(self, ast):
+        if ast == 'alpha':
+            return '⍺'
+        elif ast == '/alpha':
+            return 'alpha'
+        elif ast == 'betta':
+            return 'ᵝ'
+        elif ast == '/betta':
+            return 'betta'
+        else:
+            return ast
+
 
     def term(self, ast):
         if not isinstance(ast, tatsu.ast.AST):
             return ast
         elif ast.op == "*":
-            return ast.left * ast.right
+            return f'{ast.left} * {ast.right}'
         elif ast.op == "/":
-            return ast.left / ast.right
+            return f'{ast.left} / {ast.right}'
         elif ast.op == "**":
-            return ast.left ** ast.right
+            return f'{ast.left} ** {ast.right}'
+        elif ast.op == '=':
+            variables_dict[ast.variable_name] = ast.variable_value
+            return f'nice bro, variable with name: {ast.variable_name} and value: {ast.variable_value} and type: {type(ast.variable_value)}'
         else:
             raise Exception("я такого оператора не знаю", ast.op)
 
-    def root(self, ast):
-        if not isinstance(ast, tatsu.ast.AST):
-            return ast
-        elif ast.op == "root(":
-            return ast.right ** (1 / ast.left)
-        else:
-            raise Exception("я такого оператора не знаю", ast.op)
-
-    def math_factorial(self, ast):
-        if not isinstance(ast, tatsu.ast.AST):
-            return ast
-        elif ast.op == "!":
-            return factorial(ast.middle)
-        else:
-            raise Exception("я такого оператора не знаю", ast.op)
-
-    def math_sin(self, ast):
-        if not isinstance(ast, tatsu.ast.AST):
-            return ast
-        elif ast.op == "sin(":
-            return sin(radians(ast.middle))
-        else:
-            raise Exception("я такого оператора не знаю", ast.op)
-
-    def math_cos(self, ast):
-        if not isinstance(ast, tatsu.ast.AST):
-            return ast
-        elif ast.op == "cos(":
-            return cos(radians(ast.middle))
-        else:
-            raise Exception("я такого оператора не знаю", ast.op)
 
     def expression(self, ast):
         if not isinstance(ast, tatsu.ast.AST):
             return ast
         elif ast.op == "+":
-            return ast.left + ast.right
+            return f'({ast.left} + {ast.right})'
         elif ast.op == "-":
-            return ast.left - ast.right
+            return f'({ast.left} - {ast.right})'
+
         else:
             raise Exception("Unknown operator", ast.op)
 
 
-def parse_with_basic_semantics():
-    grammar = open("calc.ebnf").read()
+def parse_with_basic_semantics(input_file: str):
+    with open(input_file) as file:
+        grammar = open("calc.ebnf").read()
 
-    parser = tatsu.compile(grammar)
-    ast = parser.parse(input("ваше выражение: "), semantics=CalcBasicSemantics())
+        parser = tatsu.compile(grammar)
 
-    pprint(ast, width=20, indent=4)
+        #### вот тут выполнение кода из файла, оно по умолчанию идет
+        for line in file.readlines():
+            ast = parser.parse(line.replace('\n', ''), semantics=CalcBasicSemantics())
+            pprint(ast, width=20, indent=4)
+            print()
+        ####
+        
+        #### вот тут если надо выполнение кода из консольки, ну мало ли там, отлаживать что-то надо будет
+            # ast = parser.parse(input("ваше выражение: "), semantics=CalcBasicSemantics())
+            # pprint(ast, width=20, indent=4)
+        ####
+
+        
 
 
 if __name__ == "__main__":
-    parse_with_basic_semantics()
+    parse_with_basic_semantics(input('input filename: '))
+    print(f'variables dictionary:\n{variables_dict}')
